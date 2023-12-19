785dc7e1c776bfb6de377cf04765f7fa789b26fe
FMiguel95 <92217333+FMiguel95@users.noreply.github.com>
2023-12-19 23:36:16 +0000

    Add files via upload

--- CPP03/ex03/DiamondTrap.cpp
+++ CPP03/ex03/DiamondTrap.cpp
@@ -3,15 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fernacar <fernacar@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: fernacar <fernacar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 23:50:49 by fernacar          #+#    #+#             */
-/*   Updated: 2023/12/19 01:01:36 by fernacar         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:31:55 by fernacar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 #include <iostream>
+
+DiamondTrap::DiamondTrap() : ClapTrap(), ScavTrap(), FragTrap(), _name("Default")
+{
+	std::cout <<"DiamondTrap default constructor called" << std::endl;
+	ClapTrap::_name = _name + "_clap_name";
+	_hitPoints = FragTrap::_hitPoints;
+	_energyPoints = ScavTrap::_energyPoints;
+	_attackDamage = FragTrap::_attackDamage;
+}
 
 DiamondTrap::DiamondTrap(const std::string& name) : ClapTrap(name), ScavTrap(name), FragTrap(name), _name(name)
 {
@@ -20,6 +29,12 @@ DiamondTrap::DiamondTrap(const std::string& name) : ClapTrap(name), ScavTrap(nam
 	_hitPoints = FragTrap::_hitPoints;
 	_energyPoints = ScavTrap::_energyPoints;
 	_attackDamage = FragTrap::_attackDamage;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& src) : ClapTrap(src), ScavTrap(src), FragTrap(src)
+{
+	std::cout << "DiamondTrap copy constructor called" << std::endl;
+	*this = src;
 }
 
 DiamondTrap::~DiamondTrap()
--- CPP03/ex03/DiamondTrap.hpp
+++ CPP03/ex03/DiamondTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fernacar <fernacar@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: fernacar <fernacar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 23:50:39 by fernacar          #+#    #+#             */
-/*   Updated: 2023/12/19 01:03:56 by fernacar         ###   ########.fr       */
+/*   Updated: 2023/12/19 21:29:04 by fernacar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 class DiamondTrap : public ScavTrap, public FragTrap
 {
 	public:
+		DiamondTrap();
 		DiamondTrap(const std::string& name);
+		DiamondTrap(const DiamondTrap& src);
 		~DiamondTrap();
 
 		using ScavTrap::attack;
